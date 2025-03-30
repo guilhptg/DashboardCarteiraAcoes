ead195f48ea4a1dd5a67b8dc7e7d4c692befd2ac
Guilherme Portugal <flordavidaho@gmail.com>
2025-03-30 03:10:50 -0300

    update
    docker

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 import yfinance as yf
 from datetime import timedelta
 
+st.set_page_config(layout='wide')
+
+
 # criar as funções de carregamento de dados
-    # cotação do Itau (ITUB4) - 2010 a 2024
-
-
 @st.cache_data
 def carregar_dados(empresas):
     texto_tickers = " ".join(empresas)
@@ -15,6 +15,7 @@ def carregar_dados(empresas):
     return cotacoes_acao['Close']
 
 
+#tranformar os tickers em tickers Nacionais 
 @st.cache_data
 def carregar_tickers_acoes():
     base_tickers = pd.read_csv('IBOV.csv', sep=';')
@@ -64,26 +65,28 @@ st.line_chart(dados)
 
 
 #calculo performace
-texto_performace_ativos = "Rendimento X"
+texto_performace_ativos = ""
 
 if len(lista_acoes) == 0:
     lista_acoes = list(dados.columns)
 elif len(lista_acoes) == 1:
     dados = dados.rename(columns={'Close': acao_unica})
 
+# Valor Investido no Início do Periodo
+valor_investido = st.sidebar.number_input('Valor Investido (R$)', min_value=1.00)
 
 # carteira / atribuir um valor variável para carteira
-carteira = [1000 for acao in lista_acoes]
+carteira = [valor_investido for acao in lista_acoes]
 total_inicial_carteira = sum(carteira)
 
 
 
 for i, acao in enumerate(lista_acoes):
-    performace_ativo = dados[acao].iloc[0] / dados[acao].iloc[0] - 1
+    performace_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
     performace_ativo = float(performace_ativo)
 
     carteira[i] = carteira[i] * (1 + performace_ativo)
-
+    print(acao)
     if performace_ativo > 0:
         texto_performace_ativos = texto_performace_ativos + f'  \n{acao}: :green[{performace_ativo:.1%}]'
 
@@ -95,22 +98,26 @@ for i, acao in enumerate(lista_acoes):
 
 total_final_carteira = sum(carteira)
 
-performace_carteira = total_final_carteira / total_inicial_carteira - 1
+performace_carteira = (total_final_carteira / total_inicial_carteira) - 1
 
+porcentagem = (valor_investido * performace_ativo)
+total_rendido_reais = valor_investido + porcentagem
+cor_total_rendido = ":green" if total_rendido_reais >= 0 else (":red" if total_rendido_reais < 0 else ":blue")
 
-
+texto_total_rendido_reais = f'### Performance da Investimento  \nTotal: {cor_total_rendido}[R$ {total_rendido_reais:.2f}]'
 
 if performace_carteira > 0:
-    texto_performace_carteira = f'Performace da carteira com todos ativos: :green[{performace_carteira:.2%}]'
+    texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:green[{performace_carteira:.2%}]'
 
 elif performace_carteira < 0:
-    texto_performace_carteira = f'Performace da carteira com todos ativos: :red[{performace_carteira:.2%}]'
+    texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:red[{performace_carteira:.2%}]'
 
 else:
-    texto_performace_carteira = f'Performace da carteira com todos ativos: :blue[{performace_carteira:.2%}]'
+    texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:blue[{performace_carteira:.2%}]'
 
-
-st.write(f"""
+container = st.container(border=True)
+with container:
+    st.write(f"""
 ### Performace dos Ativos
 Essa foi a performace de cada ativo no período selecionado:
 
@@ -118,4 +125,7 @@ Essa foi a performace de cada ativo no período selecionado:
 
 {texto_performace_carteira}
 
-""")
+
+{texto_total_rendido_reais}
+
+    """)
