59f746563d4071da11e2c2af291243dcf1bdd619
Guilherme Portugal <flordavidaho@gmail.com>
2025-03-30 03:11:54 -0300

    update
    main.py

--- main.py
+++ main.py
@@ -5,29 +5,38 @@ from datetime import timedelta
 
 st.set_page_config(layout='wide')
 
-
-# criar as funções de carregamento de dados
 @st.cache_data
 def carregar_dados(empresas):
+    """
+    Carrega os dados históricos de fechamento das ações das empresas especificadas.
+
+    Parâmetros:
+    empresas (list of str): Lista de tickers das empresas no formato 'TICKER.SA'.
+
+    Retorna:
+    DataFrame: DataFrame contendo os preços de fechamento ajustados das ações.
+    """
     texto_tickers = " ".join(empresas)
     dados_acao = yf.Tickers(texto_tickers) # .SA faz referência a bolsa nacional (São Paulo)
     cotacoes_acao = dados_acao.history(period='1d', start='2010-01-01', end='2025-03-01')
     return cotacoes_acao['Close']
 
-
-#tranformar os tickers em tickers Nacionais 
 @st.cache_data
 def carregar_tickers_acoes():
+    """
+    Carrega a lista de tickers das ações do índice IBOV a partir de um arquivo CSV.
+
+    Retorna:
+    list of str: Lista de tickers das ações no formato 'TICKER.SA'.
+    """
     base_tickers = pd.read_csv('IBOV.csv', sep=';')
     tickers = list(base_tickers['Código'])
     tickers = [item + '.SA' for item in tickers]
     return tickers
 
-
 # Preparar vizualização
 
 acoes = carregar_tickers_acoes()
-# acoes = ['SANB11.SA', 'ITSA4.SA', 'JBSS3.SA', 'SUZB3.SA', 'VIVT3.SA']
 dados = carregar_dados(acoes)
 
 st.write("""
@@ -55,16 +64,14 @@ intervalo_datas = st.sidebar.slider('Selecione o período',
                                     max_value=data_final, 
                                     value=(data_inicial, data_final),
                                     step=timedelta(days=1))
-print(intervalo_datas)
 
 dados = dados.loc[intervalo_datas[0]:intervalo_datas[1]]
 
-
-# Criar gráfico
+# Gráfico de Linha
 st.line_chart(dados)
 
 
-#calculo performace
+# Cálculo de Performance
 texto_performace_ativos = ""
 
 if len(lista_acoes) == 0:
@@ -75,11 +82,9 @@ elif len(lista_acoes) == 1:
 # Valor Investido no Início do Periodo
 valor_investido = st.sidebar.number_input('Valor Investido (R$)', min_value=1.00)
 
-# carteira / atribuir um valor variável para carteira
+# Carteira / atribuir um valor variável para carteira
 carteira = [valor_investido for acao in lista_acoes]
 total_inicial_carteira = sum(carteira)
-
-
 
 for i, acao in enumerate(lista_acoes):
     performace_ativo = dados[acao].iloc[-1] / dados[acao].iloc[0] - 1
@@ -89,10 +94,8 @@ for i, acao in enumerate(lista_acoes):
     print(acao)
     if performace_ativo > 0:
         texto_performace_ativos = texto_performace_ativos + f'  \n{acao}: :green[{performace_ativo:.1%}]'
-
     elif performace_ativo < 0:
         texto_performace_ativos = texto_performace_ativos + f'  \n{acao}: :red[{performace_ativo:.1%}]'
-
     else:
         texto_performace_ativos = texto_performace_ativos + f'  \n{acao}: :blue[{performace_ativo:.1%}]'
 
@@ -108,10 +111,8 @@ texto_total_rendido_reais = f'### Performance da Investimento  \nTotal: {cor_tot
 
 if performace_carteira > 0:
     texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:green[{performace_carteira:.2%}]'
-
 elif performace_carteira < 0:
     texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:red[{performace_carteira:.2%}]'
-
 else:
     texto_performace_carteira = f'#### Performace da carteira com todos ativos:  \n:blue[{performace_carteira:.2%}]'
 
@@ -128,4 +129,4 @@ Essa foi a performace de cada ativo no período selecionado:
 
 {texto_total_rendido_reais}
 
-    """)
+""")
